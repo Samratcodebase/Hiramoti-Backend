8f39579a4c7af584dcdca3688884b97d246c8587
Samratcodebase <samratcodebase@gmail.com>
2026-02-27 22:22:39 +0530

    Add the server logic

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
+import dotenv from "dotenv";
 import app from "./src/app.js";
-
-
+dotenv.config();
 
 app.listen(3000, () => {
   console.log(`server is Running at 3000`);
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import type { Application } from "express";
-import { extend } from "zod/mini";
+
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1");
+// app.use("/api/v1");
 
 export default app;
