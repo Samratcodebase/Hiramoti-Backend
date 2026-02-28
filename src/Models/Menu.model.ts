45afdbcc949b05f24bf6c467bac6d908d0e0ebff
Samratcodebase <samratcodebase@gmail.com>
2026-02-28 22:24:37 +0530

    Menu Validation Added

---
+++ src/Models/Menu.model.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const menuSchema = new mongoose.Schema(
+  {
+    foodName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      default: "",
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    category: {
+      type: String, // e.g. "Starter", "Main Course"
+      required: true,
+    },
+
+    isVeg: {
+      type: Boolean,
+      default: false,
+    },
+
+    isAvailable: {
+      type: Boolean,
+      default: true,
+    },
+
+    image: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("menu", menuSchema);
---
+++ src/validators/validator.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const menuItemZod = z.object({
+  foodName: z.string().min(1, "Food name is required").trim(),
+
+  description: z.string().optional().default(""),
+
+  price: z.number().min(0, "Price must be >= 0"),
+
+  category: z.string().min(1, "Category is required"),
+
+  isVeg: z.boolean().optional().default(false),
+
+  isAvailable: z.boolean().optional().default(true),
+
+  image: z.string().optional().or(z.literal("")),
+
+
+});
