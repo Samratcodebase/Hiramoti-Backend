d5a82269d2a5be7852c559880f01b889bccb26d2
Samrat <samratcodebase@gmail.com>
2026-03-15 22:37:30 +0530

    Refactor: CodeBase CleanUp (#6)

--- src/controllers/admin.controller.ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response } from "express";
-const AddItem = async (req: Request, res: Response) => {
-  try {
-  } catch (error) {}
-};
-
-export default { AddItem };
--- src/routes/admin.routes.ts
+++
@@ -1,7 +0,0 @@
-import express, { Express } from "express";
-import AdminController from "../controllers/admin.controller.js";
-const AdminRouter = express.Router();
-
-AdminRouter.post("/item", AdminController.AddItem);
-
-export default AdminRouter;
